3c06619e030b05017da74410f64e7c7a1ed51a37
totorolin <97090522+totorolin@users.noreply.github.com>
2023-10-31 17:48:16 +0800

    Update rtmdet_m_8xb32-300e_coco.py

--- configs/rtmdet/rtmdet_m_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet_m_8xb32-300e_coco.py
@@ -4,3 +4,7 @@ model = dict(
     backbone=dict(deepen_factor=0.67, widen_factor=0.75),
     neck=dict(in_channels=[192, 384, 768], out_channels=192, num_csp_blocks=2),
     bbox_head=dict(in_channels=192, feat_channels=192))
+
+test_evaluator = dict(
+    format_only=True,
+    outfile_prefix='./work_dirs/coco_detection/rtmdet_m_val')
