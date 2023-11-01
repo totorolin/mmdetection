2590ae09119723f1b90c402168117e8c4f75f7c0
totorolin <97090522+totorolin@users.noreply.github.com>
2023-11-01 11:00:49 +0800

    Update sparse-rcnn_r101_fpn_300-proposals_crop-ms-480-800-3x_coco.py

--- configs/sparse_rcnn/sparse-rcnn_r101_fpn_300-proposals_crop-ms-480-800-3x_coco.py
+++ configs/sparse_rcnn/sparse-rcnn_r101_fpn_300-proposals_crop-ms-480-800-3x_coco.py
@@ -5,3 +5,7 @@ model = dict(
         depth=101,
         init_cfg=dict(type='Pretrained',
                       checkpoint='torchvision://resnet101')))
+
+test_evaluator = dict(
+    format_only=True,
+    outfile_prefix='./work_dirs/coco_detection/sparse_rcnn_val')
